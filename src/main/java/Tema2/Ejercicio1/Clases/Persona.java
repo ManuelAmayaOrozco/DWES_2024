84a3b3b41635d019e7c0b4b6eb7a3793921bac72
ManuelAmayaOrozco <mamaoro1405@g.educaand.es>
2024-09-25 21:37:21 +0200

    Ejercicio persona

---
+++ src/main/java/Tema2/Ejercicio1/Clases/Persona.java
@@ -0,0 +1,14 @@
+package Tema2.Ejercicio1.Clases;
+
+public class Persona {
+    public String nombre;
+    public String contrasenia;
+
+    public Persona (String nombre, String contrasenia) {
+
+        this.nombre = nombre;
+        this.contrasenia = contrasenia;
+
+    }
+
+}
