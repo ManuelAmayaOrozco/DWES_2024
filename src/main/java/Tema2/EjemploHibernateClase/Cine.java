26ca78604c0de8d8f8b8c03f1685ff974957ad68
ManuelAmayaOrozco <mamaoro1405@g.educaand.es>
2024-10-15 19:33:48 +0200

    Ejemplo Hibernate 2

--- src/main/java/Tema2/EjemploHibernateClase/Cine.java
+++ src/main/java/Tema2/EjemploHibernateClase/Cine.java
@@ -1,21 +1,66 @@
 package Tema2.EjemploHibernateClase;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
-@Table
+@Table(name = "cine")
 public class Cine {
 
     @Id
-    private String id;
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Column
+    @Column (name = "nombre", nullable = false, length = 50)
     private String nombre;
 
-    @Column
-    private Integer nAsientos;
+    @Column (name = "capacidad", nullable = false)
+    private int capacidad;
 
+    @Column (name = "direccion", nullable = false)
+    private String direccion;
+
+    public Cine() {
+
+
+
+    }
+
+    public Cine(Long id, String nombre, int capacidad, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+        this.direccion = direccion;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
 }
--- src/main/java/Tema2/EjemploHibernateClase/Pelicula.java
+++ src/main/java/Tema2/EjemploHibernateClase/Pelicula.java
@@ -1,21 +1,81 @@
 package Tema2.EjemploHibernateClase;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
-@Table
+@Table (name = "peliculas")
 public class Pelicula {
 
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Column
-    private String nombre;
+    @Column (name = "titulo", length = 50, unique = true, nullable = false)
+    private String titulo;
 
-    @Column
+    @Column (name = "autor", length = 50, nullable = false)
     private String autor;
 
+    @Transient
+    private String autores;
+
+    @Column (name = "fecha_lanzamiento", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date fechaLanzamiento;
+
+    public Pelicula() {
+
+
+
+    }
+
+    public Pelicula(Long id, String titulo, String autor, String autores, Date fechaLanzamiento) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.autores = autores;
+        this.fechaLanzamiento = fechaLanzamiento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getAutores() {
+        return autores;
+    }
+
+    public void setAutores(String autores) {
+        this.autores = autores;
+    }
+
+    public Date getFechaLanzamiento() {
+        return fechaLanzamiento;
+    }
+
+    public void setFechaLanzamiento(Date fechaLanzamiento) {
+        this.fechaLanzamiento = fechaLanzamiento;
+    }
 }
